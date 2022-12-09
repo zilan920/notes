d4c7af9862e98de0564e9fb938a8169327cbc658
zihan.fang <zihan.fang@bybit.com>
2022-12-09 15:23:51 +0800

    files and notes

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -3,6 +3,12 @@ import { sidebar } from "vuepress-theme-hope";
 // 精选图标：https://vuepress-theme-hope.github.io/v2/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default sidebar([
   {
+    text: "探索",
+    icon: "icon-back-stage",
+    prefix: "",
+    link: "/contents/bookmark/Guide.md",
+  },
+  {
     text: "学习记录",
     icon: "icon-back-stage",
     prefix: "/contents/learning/",
@@ -21,5 +27,11 @@ export default sidebar([
         link: "golang.md",
       },
     ],
+  },
+  {
+    text: "阅读",
+    icon: "icon-back-stage",
+    prefix: "",
+    link: "/contents/Books.md",
   },
 ]);
---
+++ docs/contents/bookmark/Guide.md
@@ -0,0 +1,8 @@
+---
+article: false
+title: 学习记录
+icon: read
+---
+
+### 学习链接
+
